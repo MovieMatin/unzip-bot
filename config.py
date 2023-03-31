40c22d1b2704ef2a32bca5a57506df8328f1bf51
Ashrafmdmatin41 <91468485+Ashrafmdmatin41@users.noreply.github.com>
2023-04-01 01:30:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ import os
 
 
 class Config:
-    APP_ID = int(os.environ.get("APP_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL"))
-    MONGODB_URL = os.environ.get("MONGODB_URL")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    APP_ID = int(os.environ.get("15236804"))
+    API_HASH = os.environ.get("409da5b68ad699091fa72b381921f0e5")
+    BOT_TOKEN = os.environ.get("5612635073:AAF03jY6fawSLM9BGYpNB8_JKdnDlgyr-qs")
+    LOGS_CHANNEL = int(os.environ.get("-1001684353224"))
+    MONGODB_URL = os.environ.get("mongodb+srv://mdmatinnew:Hg72p3iCsni9Olbf@cluster0.j2hvxhj.mongodb.net/?retryWrites=true&w=majority")
+    BOT_OWNER = int(os.environ.get("1963114305"))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/Downloaded"
     THUMB_LOCATION = f"{os.path.dirname(__file__)}/Thumbnails"
     TG_MAX_SIZE = 2040108421
