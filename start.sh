5124e839a3e98d71fb23a5be5dd3152b2ae267a7
Ashrafmdmatin41 <91468485+Ashrafmdmatin41@users.noreply.github.com>
2023-04-01 01:31:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 echo "
 🔥 Unzip Bot 🔥
 
-Copyright (c) 2023 EDM115
+Copyright (c) 2023 Matiz
 
---> Join @EDM115bots
---> Follow EDM115 on Github
+--> Join @Movie_Matin
+--> Follow Matiz Techz on Telegram
 "
 python3 -m unzipper
