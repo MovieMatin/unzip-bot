39625ef0df6a358f04b5eff4c1a595fb6214e660
MovieMatin <125181425+MovieMatin@users.noreply.github.com>
2023-04-01 10:38:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     APP_ID = int(os.environ.get 15236804
     API_HASH = os.environ.get("409da5b68ad699091fa72b381921f0e5")
     BOT_TOKEN = os.environ.get("5612635073:AAF03jY6fawSLM9BGYpNB8_JKdnDlgyr-qs")
-    LOGS_CHANNEL = int(os.environ.get"-1001684353224"
+    LOGS_CHANNEL = int(os.environ.get("-1001684353224"))
     MONGODB_URL = os.environ.get("mongodb+srv://mdmatinnew:Hg72p3iCsni9Olbf@cluster0.j2hvxhj.mongodb.net/?retryWrites=true&w=majority")
     BOT_OWNER = int(os.environ.get("1963114305"))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/Downloaded"
